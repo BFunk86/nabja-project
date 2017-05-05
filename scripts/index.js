bac0654315dd8c41adb3390b908b97ecfd7fa58d
Brandon Young <youngbd01@sunyjcc.edu>
2017-05-05 16:28:04 -0400

    added modal

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@
 var modal = document.getElementById('modal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("login");
+var btn = document.getElementById("loginBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -15,18 +15,18 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
-}
+};
 
-// When the user clicks on <span> (x), close the modal
+// Closes the modal when user clicks close
 span.onclick = function() {
     modal.style.display = "none";
-}
+};
 
-// When the user clicks anywhere outside of the modal, close it
+// This closes the modal if the user clicks outside of it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
  
 console.log("Created by the Web Development Team");
